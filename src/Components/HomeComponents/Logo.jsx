a9e28ebdd00559f419c30d35fccdc1b9fc3cf0b0
Usman Ali <Usman.tpr@gmail.com>
2024-10-11 20:26:34 +0500

    updated

--- src/Components/HomeComponents/Logo.jsx
+++ src/Components/HomeComponents/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
     return (
         <>
-      <div className='flex space-x-4 items-center justify-center'>
+      <Link to='/' className='flex space-x-4 items-center justify-center'>
       <img src="/assets/Logo/Preview.png" alt="logo" className='w-14 rounded-full'/>
       <p className='text-black font-bold'>Gold Bazaar</p>
-      </div>
+      </Link>
 
         </>
     )
